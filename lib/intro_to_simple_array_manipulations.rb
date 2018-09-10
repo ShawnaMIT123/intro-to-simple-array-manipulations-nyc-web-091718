8cd0daac36a48d11df3d955afa780b0bab7c23de
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-10 18:07:26 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,28 +2,26 @@ def using_push(array, string)
   array.push(string)
 end
  
-  using_push
-  takes in two arguments, an array and a string and adds that string to the endof the array using the push method (FAILED - 1)
+def using_unshift(array, string)
+  array.unshift(string)
+end
+ 
+def using_pop(array)
+  array.pop
+end
 
-#using_unshift
-  takes in two arguments, an array and a string and adds that string to the front of the array using the unshift method (FAILED - 2)
-  increases the length of the array (FAILED - 3)
+def pop_with_args(array)
+  array.pop(2)
+end
 
-#using_pop
-  takes in argument of an array and uses the pop method to remove the last element from the array and return that element (FAILED - 4)
-  decreases the length of the array by 1 (FAILED - 5)
+def using_shift(array)
+  array.unshift
+end
+  
 
-#pop_with_args
-  takes in an argument of an array and uses the pop method with an argument of 2 to remove the last two array items and return them (FAILED - 6)
-  decreases the length of the array by 2 (FAILED - 7)
-
-#using_shift
-  takes in an argument of an array and uses the shift method to remove the first item and return it (FAILED - 8)
-  decreases the length of the array by 1 (FAILED - 9)
-
-#shift_with_args
-  takes in an argument of an array and uses the shift method with an argument of 2 to remove and return the first 2 items from the array (FAILED - 10)
-  decreases the length of the array by 2 (FAILED - 11)
+def shift_with_args(array)
+  array.shift(2)
+end
 
 #using_concat
   takes in two arguments of two different arrays and uses the concat method to add the contents of the second array to the first (FAILED - 12)

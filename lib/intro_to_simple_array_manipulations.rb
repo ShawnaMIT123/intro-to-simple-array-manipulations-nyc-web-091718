11afc3f2c78ae166a3ad3bbec9675fc1b4094572
Shawna Davis <shawna_davis@alum.mit.edu>
2018-09-10 18:12:07 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,21 +23,26 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-#using_concat
-  takes in two arguments of two different arrays and uses the concat method to add the contents of the second array to the first (FAILED - 12)
-  increases the length of the first array (FAILED - 13)
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
 
-#using_insert
-  takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array (FAILED - 14)
-  increases the length of the array (FAILED - 15)
+def using_insert(array, element)
+  array.insert(element, 3)
+end
+def using_uniq(array)
+  array.uniq
+end
 
-#using_uniq
-  takes in an argument of an array and uses the uniq method to remove any duplicate items (FAILED - 16)
 
-#using_flatten
-  takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings (FAILED - 17)
+def using_flatten(array)
+  array.flatten
+end
 
-#using_delete
-  takes in two arguments, an array and a string, and uses the delete method to remove any items from the array that are equal to that string (FAILED - 18)
+def using_delete(array, string)
+  array.delete(string)
+end
 
-#using_delete_at
+
+def using_delete_at
+end
